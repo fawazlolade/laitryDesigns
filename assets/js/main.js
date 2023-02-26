18b9aa0616d7c866ee22470d4dc4389c0dd1bf40
Fawazlolade <fawazdeenabimbola@gmail.com>
2023-02-25 19:25:01 -0500

    created a js file to write script for the website

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+<header>
+<nav>
+    <div class="left">
+
+    </div>
+    <div class="center">
+
+    </div>
+    <div class="right">
+    <a href=""><i class="uil uil-envelope-alt"></i></a>
+             
+                
+             <a href=""><i class="uil uil-phone-alt"></i></a>
+         
+             <a href=""><i class="uil uil-map-pin-alt"></i></a>
+    </div>
+</nav>
